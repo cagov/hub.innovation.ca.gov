c35e201ac0f535bd47e1244c89cfda083c69d133
Jim Bumgardner <jbum@jbum.com>
2026-06-23 15:20:05 -0700

    Removed icons on format labels, and added topic labels to destination pages.

--- docs/pages/pages.11tydata.js
+++ docs/pages/pages.11tydata.js
@@ -1,7 +1,23 @@
+const TOPIC_LABELS = {
+  'customer-experience': 'Customer experience',
+  'content-design': 'Content design',
+  data: 'Data',
+  'human-centered-design': 'Human-centered design',
+  'product-management': 'Product management',
+};
+
 module.exports = {
   layout: 'page',
   eleventyComputed: {
     permalink: (article) =>
       `${article.page.filePathStem.replace('/docs/pages', '')}/index.html`,
+    // Topic label shown beside the format label on content pages. Defaults to
+    // the page's top-level topic directory; a page may override via front matter.
+    topiclabel: (data) => {
+      if (data.topiclabel) return data.topiclabel;
+      const stem = data.page.filePathStem.replace('/docs/pages/', '');
+      const topDir = stem.split('/')[0];
+      return TOPIC_LABELS[topDir] || '';
+    },
   },
 };
